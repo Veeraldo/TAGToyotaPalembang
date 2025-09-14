442f75abf1f2b265019ee6ac710910db40dc3b45
Veeraldo <Veraldo24092005@gmail.com>
2025-09-14 20:05:12 +0700

    HomeScreen

--- tagtoyota/lib/screen/home_screen.dart
+++ tagtoyota/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/intl.dart';
+import 'package:tagtoyota/screen/profile_screen.dart';
+import 'package:tagtoyota/screen/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-
   int _currentIndex = 0;
- final List<IconData> _icons = [
+
+  final List<IconData> _icons = [
     Icons.home,
     Icons.search,
     Icons.person,
@@ -25,82 +24,59 @@ class _HomeScreenState extends State<HomeScreen> {
     "Search",
     "Profile",
   ];
+
+  late final List<Widget> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      const HomeContent(),
+      const SearchScreen(),
+      const ProfileScreen(),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // AppBar custom
       appBar: PreferredSize(
-  preferredSize: const Size.fromHeight(50),
-  child: Container(
-    decoration: BoxDecoration(
-      color: Colors.white,
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black26,
-          blurRadius: 6,
-          offset: const Offset(0, 3),
-        ),
-      ],
-      borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(20),
-        bottomRight: Radius.circular(20),
-      ),
-    ),
-    child: SafeArea(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset(
-            "assets/TAGNyamping.png",
-            height: 50, 
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
-    ),
-  ),
-),
-
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Kalender realtime
-            TableCalendar(
-              locale: 'en_US',
-              firstDay: DateTime.utc(2000, 1, 1),
-              lastDay: DateTime.utc(9999, 12, 31),
-              focusedDay: _focusedDay,
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-              },
-              headerStyle: const HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
+        preferredSize: const Size.fromHeight(50),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: const Offset(0, 3),
               ),
-              calendarStyle: const CalendarStyle(
-                todayDecoration: BoxDecoration(
-                  color: Color.fromARGB(255, 22, 22, 22),
-                  shape: BoxShape.circle,
-                ),
-                selectedDecoration: BoxDecoration(
-                  color: Color.fromARGB(255, 255, 0, 0),
-                  shape: BoxShape.circle,
+            ],
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          child: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  "assets/TAGNyamping.png",
+                  height: 50,
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
-          ],
+          ),
         ),
       ),
 
+      body: _screens[_currentIndex],
+
       // Bottom Navbar
-     bottomNavigationBar: Container(
+      bottomNavigationBar: Container(
         decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: [
@@ -130,9 +106,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
                   curve: Curves.easeOut,
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
                   decoration: BoxDecoration(
-                    color: isSelected ? Colors.red.withOpacity(0.1) : Colors.transparent,
+                    color: isSelected
+                        ? Colors.red.withOpacity(0.1)
+                        : Colors.transparent,
                     borderRadius: BorderRadius.circular(12),
                   ),
                   child: Column(
@@ -151,26 +130,26 @@ class _HomeScreenState extends State<HomeScreen> {
                       const SizedBox(height: 4),
                       AnimatedSwitcher(
                         duration: const Duration(milliseconds: 300),
-                        transitionBuilder: (child , animation) =>FadeTransition(
+                        transitionBuilder: (child, animation) => FadeTransition(
                           opacity: animation,
                           child: SizeTransition(
                             sizeFactor: animation,
                             axis: Axis.horizontal,
                             child: child,
-                          ),),
-                          child: isSelected ? 
-                          Text(
-                            _labels[index],
-                            key: ValueKey(index),
-                            style: const TextStyle(
-                              color: Color(0xFFFE0000),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
                           ),
+                        ),
+                        child: isSelected
+                            ? Text(
+                                _labels[index],
+                                key: ValueKey(index),
+                                style: const TextStyle(
+                                  color: Color(0xFFFE0000),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              )
+                            : const SizedBox.shrink(),
                       )
-                      : const SizedBox.shrink(),
-                      )
-                    
                     ],
                   ),
                 ),
@@ -178,6 +157,59 @@ class _HomeScreenState extends State<HomeScreen> {
             }),
           ),
         ),
+      ),
+    );
+  }
+}
+
+
+class HomeContent extends StatefulWidget {
+  const HomeContent({super.key});
+
+  @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TableCalendar(
+            locale: 'en_US',
+            firstDay: DateTime.utc(2000, 1, 1),
+            lastDay: DateTime.utc(9999, 12, 31),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+            ),
+            calendarStyle: const CalendarStyle(
+              todayDecoration: BoxDecoration(
+                color: Color.fromARGB(255, 22, 22, 22),
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Color.fromARGB(255, 255, 0, 0),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
